bb2e84f56e6ebed2d78514d179cf7e2082a813e6
jyj5457 <154506328+jyj5457@users.noreply.github.com>
2024-01-12 09:20:40 +0900

    [level Lv.0] Title: 두 수의 차, Time: 0.13 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120803. 두 수의 차/두 수의 차.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer;
+        answer = num1 - num2;
+        
+        num1 = 2;
+        num2 = 3;
+        return answer;
+        
+        num1 = 100;
+        num2 = 2;
+        return answer;
+    }
+}
