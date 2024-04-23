dc08ca3c5e1957dfb0fed3045970b3e9b42cc868
jyj5457 <154506328+jyj5457@users.noreply.github.com>
2024-04-23 20:17:49 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.28 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Solution {
+    public int[] solution(string[] wallpaper) {
+        int top = 50;
+        int left = 50;
+        int right = 0;
+        int bottom = 0;
+        
+        for(int i = 0; i < wallpaper.Length; i++)
+        {
+            string str = wallpaper[i];
+            for(int j = 0; j < str.Length; j++)
+            {
+                if(str[j] == '#')
+                {
+                    top = top > i ? i : top;
+                    left = left > j ? j : left;
+                    right = right < j ? j : right;
+                    bottom = bottom < i ? i : bottom;
+                }
+            }
+        }
+        int[] answer = new int[4] {top, left, bottom + 1, right +1};
+        return answer;
+    }
+}
